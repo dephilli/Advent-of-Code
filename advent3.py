3328736d18049e4c0a4ca021d69f340cf9a6f014
David Phillips <dephilli@gmail.com>
2023-03-06 23:52:17 -0900

    Update advent3.py

--- advent3.py
+++ advent3.py
@@ -30,3 +30,29 @@ def depacker(item):
 
 
 sum([depacker(i) for i in rucks ])
+#%%
+
+def common(itera):
+
+    commoner=[]
+    for i in rucks[itera]:
+        for j in rucks[itera+1]:
+            if i==j and i not in commoner:
+                commoner.append(i)
+    for k in commoner:
+        for l in rucks[itera+2]:
+            if k==l:
+                return letter_priority[l]        
+    
+common(0)
+#%%
+
+id_priority=[]
+
+id_start=0
+
+while id_start<300:
+    id_priority.append(common(id_start))
+    id_start+=3
+
+sum(id_priority)
